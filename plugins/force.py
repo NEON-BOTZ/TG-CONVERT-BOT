e4f5c5041deda24b8201c8563a1ed7f6c4012d7f
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-10 16:04:09 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -4,10 +4,7 @@ logger = logging.getLogger(__name__)
 import os
 import datetime
 
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+from config import Config
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
